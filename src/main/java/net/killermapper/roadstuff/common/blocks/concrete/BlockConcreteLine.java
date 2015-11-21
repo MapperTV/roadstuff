e8bce3b0a093dabe4dba714c88d11b6a26b80a46
KillerMapper <killermapper@users.noreply.github.com>
2015-11-21 23:50:05 +0100

    Added concrete road blocks

--- src/main/java/net/killermapper/roadstuff/common/blocks/concrete/BlockConcreteLine.java
+++ src/main/java/net/killermapper/roadstuff/common/blocks/concrete/BlockConcreteLine.java
@@ -1,15 +1,20 @@
 /*
 Road Stuff - A Minecraft MODification by KillerMapper - 2015
+
 The MIT License (MIT)
+
 Copyright (c) 2015 KillerMapper
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-package net.killermapper.roadstuff.common.blocks;
+package net.killermapper.roadstuff.common.blocks.concrete;
 
 import java.util.List;
 
@@ -38,13 +43,13 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
-public class BlockAsphaltLines01 extends Block
+public class BlockConcreteLine extends Block
 {
 
-    public static String[] subBlock = new String[] {"simplewhiteline", "simpleyellowline", "largewhiteline", "largeyellowline", "doublewhiteline", "doubleyellowline"};
-    private IIcon asphaltBase, simpleWhiteLine, simpleYellowLine, largeWhiteLine, largeYellowLine, doubleWhiteLine, doubleYellowLine;
+    public static String[] subBlock = new String[] {"concretesimplewhiteline", "concretesimpleyellowline", "concretelargewhiteline", "concretelargeyellowline", "concretedoublewhiteline", "concretedoubleyellowline"};
+    private IIcon concreteBase, simpleWhiteLine, simpleYellowLine, largeWhiteLine, largeYellowLine, doubleWhiteLine, doubleYellowLine;
 
-    public BlockAsphaltLines01()
+    public BlockConcreteLine()
     {
         super(Material.rock);
         this.setCreativeTab(RoadStuff.RoadStuffCreativeTabs);
@@ -83,13 +88,13 @@ public class BlockAsphaltLines01 extends Block
 
     public void registerBlockIcons(IIconRegister iconRegister)
     {
-        this.asphaltBase = iconRegister.registerIcon(RoadStuff.MODID + ":asphaltBase");
-        this.simpleWhiteLine = iconRegister.registerIcon(RoadStuff.MODID + ":asphaltSimpleWhiteLine");
-        this.simpleYellowLine = iconRegister.registerIcon(RoadStuff.MODID + ":asphaltSimpleYellowLine");
-        this.largeWhiteLine = iconRegister.registerIcon(RoadStuff.MODID + ":asphaltLargeWhiteLine");
-        this.largeYellowLine = iconRegister.registerIcon(RoadStuff.MODID + ":asphaltLargeYellowLine");
-        this.doubleWhiteLine = iconRegister.registerIcon(RoadStuff.MODID + ":asphaltDoubleWhiteLine");
-        this.doubleYellowLine = iconRegister.registerIcon(RoadStuff.MODID + ":asphaltDoubleYellowLine");
+        this.concreteBase = iconRegister.registerIcon(RoadStuff.MODID + ":concrete/concreteBase");
+        this.simpleWhiteLine = iconRegister.registerIcon(RoadStuff.MODID + ":concrete/concreteSWL");
+        this.simpleYellowLine = iconRegister.registerIcon(RoadStuff.MODID + ":concrete/concreteSYL");
+        this.largeWhiteLine = iconRegister.registerIcon(RoadStuff.MODID + ":concrete/concreteLWL");
+        this.largeYellowLine = iconRegister.registerIcon(RoadStuff.MODID + ":concrete/concreteLYL");
+        this.doubleWhiteLine = iconRegister.registerIcon(RoadStuff.MODID + ":concrete/concreteDWL");
+        this.doubleYellowLine = iconRegister.registerIcon(RoadStuff.MODID + ":concrete/concreteDYL");
     }
 
     public IIcon getIcon(int side, int metadata)
@@ -121,7 +126,7 @@ public class BlockAsphaltLines01 extends Block
                 return this.doubleYellowLine;
             }
         }
-        return this.asphaltBase;
+        return this.concreteBase;
     }
 
     public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase living, ItemStack stack)
