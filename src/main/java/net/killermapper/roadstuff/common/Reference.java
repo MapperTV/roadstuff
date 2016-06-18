db6c02af25e08b377cceb9848b497270ea024a34
KillerMapper <killermapper@outlook.com>
2016-06-18 17:36:05 +0200

    More rectangular signs

--- src/main/java/net/killermapper/roadstuff/common/Reference.java
+++ src/main/java/net/killermapper/roadstuff/common/Reference.java
@@ -6,5 +6,5 @@ public class Reference
     public static final short maxSignSquare = 23;
     public static final short maxSignCircle = 28;
     public static final short maxSignTriangle = 25;
-    public static final short maxSignRectangle = 17;
+    public static final short maxSignRectangle = 43;
 }
