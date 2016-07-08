260059c45464ba9e07d4414038f9cfbe313eedd5
KillerMapper <killermapper@outlook.com>
2016-07-09 00:06:37 +0200

    Add few more signs

--- src/main/java/net/killermapper/roadstuff/common/Reference.java
+++ src/main/java/net/killermapper/roadstuff/common/Reference.java
@@ -2,11 +2,13 @@ package net.killermapper.roadstuff.common;
 
 public class Reference
 {
+    //The number of each different signs, for each shape
+    
     public static final short maxSignDiamond = 108;
     public static final short maxSignSquare = 64;
     public static final short maxSignCircle = 46;
     public static final short maxSignTriangle = 41;
-    public static final short maxSignRectangle = 49;
+    public static final short maxSignRectangle = 53;
     public static final short maxSignMisc = 20;
     public static final short maxSignMiscB = 7;
 }
