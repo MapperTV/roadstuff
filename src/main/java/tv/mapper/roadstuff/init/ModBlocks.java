0b5bb6fd3167481e7698dbd1189ab295448800a0
Mapper <killermapper@outlook.com>
2019-03-28 00:31:07 +0100

    First block + item group

---
+++ src/main/java/tv/mapper/roadstuff/init/ModBlocks.java
@@ -0,0 +1,31 @@
+package tv.mapper.roadstuff.init;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
+import tv.mapper.roadstuff.item.ModItemGroups;
+
+@EventBusSubscriber(bus = Bus.MOD)
+public class ModBlocks
+{
+    @ObjectHolder("roadstuff:asphalt_block")
+    public static Block ASPHALT_BLOCK;
+
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event)
+    {
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F)).setRegistryName("asphalt_block"));
+    }
+    
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(new ItemBlock(ASPHALT_BLOCK, new Item.Properties().group(ModItemGroups.ROADSTUFF)).setRegistryName(ASPHALT_BLOCK.getRegistryName()));
+    }
+}
---
+++ src/main/java/tv/mapper/roadstuff/item/ModItemGroups.java
@@ -0,0 +1,17 @@
+package tv.mapper.roadstuff.item;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import tv.mapper.roadstuff.init.ModBlocks;
+
+public class ModItemGroups
+{
+    public static final ItemGroup ROADSTUFF = new ItemGroup("roadstuff_group")
+    {
+        @Override
+        public ItemStack createIcon()
+        {
+            return new ItemStack(ModBlocks.ASPHALT_BLOCK);
+        }
+    };
+}
