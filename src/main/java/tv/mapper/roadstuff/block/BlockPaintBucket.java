42e32d0554a7edd2960453f4228ccc269baa52e9
Mapper <killermapper@outlook.com>
2019-04-20 01:00:34 +0200

    New paint bucket model + fitting collisions

--- src/main/java/tv/mapper/roadstuff/block/BlockPaintBucket.java
+++ src/main/java/tv/mapper/roadstuff/block/BlockPaintBucket.java
@@ -7,12 +7,15 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import tv.mapper.roadstuff.item.ItemBrush;
 
 public class BlockPaintBucket extends Block
 {
-
+    private static final VoxelShape BUCKET = Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 10.0D, 12.0D);
+    
     public BlockPaintBucket(Properties properties)
     {
         super(properties);
@@ -32,6 +35,16 @@ public class BlockPaintBucket extends Block
     public BlockRenderLayer getRenderLayer()
     {
         return BlockRenderLayer.CUTOUT;
+    }
+    
+    public VoxelShape getShape(IBlockState state, IBlockReader worldIn, BlockPos pos)
+    {
+        return BUCKET;
+    }
+    
+    public VoxelShape getCollisionShape(IBlockState state, IBlockReader worldIn, BlockPos pos)
+    {
+        return BUCKET;
     }
 
     public boolean onBlockActivated(IBlockState state, World worldIn, BlockPos pos, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ)
