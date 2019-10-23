be1cb0b98f3249127f4e0083954fe65b1f31ad1f
Mapper <killermapper@outlook.com>
2019-10-23 15:33:48 +0200

    More work on bollards.

--- src/main/java/tv/mapper/roadstuff/block/BollardBlock.java
+++ src/main/java/tv/mapper/roadstuff/block/BollardBlock.java
@@ -20,8 +20,11 @@ import net.minecraftforge.common.ToolType;
 
 public class BollardBlock extends RotatableBlock implements IWaterLoggable
 {
-    private static final VoxelShape EUR_BOLLARD = Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 10.0D, 16.0D, 9.0D);
-    
+    private static final VoxelShape EUR_BOLLARD_N = Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 10.0D, 16.0D, 9.0D);
+    private static final VoxelShape EUR_BOLLARD_E = Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 16.0D, 10.0D);
+    private static final VoxelShape EUR_BOLLARD_S = Block.makeCuboidShape(6.0D, 0.0D, 7.0D, 9.0D, 16.0D, 9.0D);
+    private static final VoxelShape EUR_BOLLARD_W = Block.makeCuboidShape(7.0D, 0.0D, 6.0D, 9.0D, 16.0D, 9.0D);
+
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public BollardBlock(Properties properties)
@@ -29,7 +32,7 @@ public class BollardBlock extends RotatableBlock implements IWaterLoggable
         super(properties);
         this.setDefaultState(this.getDefaultState().with(DIRECTION, Direction.NORTH).with(WATERLOGGED, Boolean.valueOf(false)));
     }
-    
+
     public BollardBlock(Properties properties, ToolType toolType)
     {
         super(properties, toolType);
@@ -45,7 +48,19 @@ public class BollardBlock extends RotatableBlock implements IWaterLoggable
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
     {
-        return EUR_BOLLARD;
+        switch((Direction)state.get(DIRECTION))
+        {
+            case NORTH:
+                return EUR_BOLLARD_N;
+            case EAST:
+                return EUR_BOLLARD_E;
+            case SOUTH:
+                return EUR_BOLLARD_S;
+            case WEST:
+                return EUR_BOLLARD_W;
+            default:
+                return EUR_BOLLARD_N;
+        }
     }
 
     public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos)
